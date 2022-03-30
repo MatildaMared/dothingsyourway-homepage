2ab0b6437596b5bf6399f82ba12cd64fc8436394
Matilda Mared <matildamared@live.se>
2022-03-30 17:42:32 +0200

    add dummy testmonials data and outline of component

--- src/components/Testimonials/Testimonials.tsx
+++ src/components/Testimonials/Testimonials.tsx
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import testimonialsData from "../../data/testimonials";
+import { User } from "react-feather";
 
 function Testimonials() {
+	const [testimonials, setTestimonials] = useState(testimonialsData);
+	const [currentCenterIndex, setCurrentCenterIndex] = useState<Number>(2);
 	return (
 		<section>
 			<h1>Testimonials</h1>
+			<List>
+				{testimonials.map((testimonial, index) => (
+					<Item
+						key={testimonial.id}
+						className={index === currentCenterIndex ? "active" : ""}
+					>
+						<UserImageWrapper>
+							<User size={36} color="#DDDDDD" />
+						</UserImageWrapper>
+						<h2>{testimonial.name}</h2>
+						<p>{testimonial.text}</p>
+					</Item>
+				))}
+			</List>
+			<button>Previous</button>
+			<button>Next</button>
 		</section>
 	);
 }
+
+const List = styled.ul`
+	list-style: none;
+	display: flex;
+	border: 1px solid gray;
+	padding: 1rem;
+`;
+
+const Item = styled.li`
+	border: 1px solid pink;
+	padding: 1rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+
+	&.active {
+		border: 1px solid purple;
+	}
+`;
+
+const UserImageWrapper = styled.div`
+	background-color: #b6b6b6;
+	width: 50px;
+	height: 50px;
+	border-radius: 50%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`
 
 export default Testimonials;
---
+++ src/data/testimonials.js
@@ -0,0 +1,34 @@
+const testimonials = [
+	{
+		id: "F205E8EB-DD3F-40D0-9173-D9CF63A314BB",
+		name: "Lars Larsson",
+		picture: "blabla",
+		text: "This is the best course I've ever taken. Markus is fantastic!",
+	},
+	{
+		id: "44A2D286-053A-4954-84C0-3373F34F7EC8",
+		name: "Greta Gustafsson",
+		picture: "blabla",
+		text: "I'm so happy I took this course. Markus is awesome!",
+	},
+	{
+		id: "E56F117E-1831-416F-B2E7-DB9073E999CA",
+		name: "Sara Svensson",
+		picture: "blabla",
+		text: "Money well spent, I will take this course again!",
+	},
+	{
+		id: "FD75616F-7D36-46F2-839C-A0107E3A5A26",
+		name: "Emil Eriksson",
+		picture: "blabla",
+		text: "This is the most awesome thing I've ever done. Markus is a great coach!",
+	},
+	{
+		id: "3AFDF680-6BA7-404B-B3D9-D92BF724DB97",
+		name: "Ida Ivarsson",
+		picture: "blabla",
+		text: "I love Markus way of teaching things. I can recommend him to anyone!",
+	},
+];
+
+export default testimonials;
