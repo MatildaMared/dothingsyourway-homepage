b5d11b9df95e137933f0bace2d18aefea41f24af
Matilda Mared <matildamared@live.se>
2022-04-17 21:09:20 +0200

    add content

--- src/components/SectionHeader/SectionHeader.tsx
+++ src/components/SectionHeader/SectionHeader.tsx
@@ -1,15 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
 interface Props {
     text: string;
 }
 
 const SectionHeader = ({ text }: Props) => {
-    return (
-        <div>
-            <h2>{text}</h2>
-        </div>
-    );
+    return <Wrapper>{text}</Wrapper>;
 };
 
 export default SectionHeader;
+
+const Wrapper = styled.h2`
+    font-size: var(--font-xl);
+    line-height: 1.8;
+    padding-block: 8rem;
+    background-color: var(--color-primary-dark);
+    margin-block-end: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: #fff;
+
+    &::after,
+    &::before {
+        content: "";
+        display: block;
+        width: 50px;
+        height: 1px;
+        background-color: rgba(255, 255, 255, .4);
+    }
+`;
--- src/pages/SalesPage.tsx
+++ src/pages/SalesPage.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Quote from "../components/Quote";
+import SectionHeader from "../components/SectionHeader";
 
 const MyComponent = () => {
     return (
         <div>
             <h1>Sales page</h1>
+            <SectionHeader text="Om mig" />
+            <Quote author="Test">
+                Min stora passion är att hjälpa människor att leva det liv de
+                alltid har drömt om.
+            </Quote>
         </div>
     );
 };
