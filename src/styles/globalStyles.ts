9cf5c1655cd7d3bafc600497d3b52a4bf3d14b61
Matilda Mared <matildamared@live.se>
2022-04-18 07:48:08 +0200

    add global styling for paragraphs

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -4,9 +4,15 @@ const GlobalStyle = createGlobalStyle`
   body {
     background: var(--color-white);
     font-family: var(--font-primary);
+    font-weight: 200;
     color: var(--color-black);
     font-size: var(--font-base);
     line-height: 1.6;
+  }
+
+  p {
+      font-family: var(--font-primary-thin);
+      margin-block-end: 1.5rem;
   }
 
     h1, h2, h3, h4, h5, h6 {
